6160991dac7570a84624a11ea53e8a111891a3a0
OGBeas222 <zholudev111@gmail.com>
2022-01-28 14:18:20 +0300

    fixed drop-down filter and added responsive for header

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,40 @@
 $(document).ready(function () {
   $(document).on("click", ".search-info__input", function () {
       $(this).toggleClass(" search-info__content--clicked");
+ 
   });
+
+
+  let select = function () {
+    let selectHeader = document.querySelectorAll('.select-head');
+    let selectItem = document.querySelectorAll('.select-item');
+  
+    selectHeader.forEach(item => {
+        item.addEventListener('click', selectToggle)
+    });
+  
+    selectItem.forEach(item => {
+        item.addEventListener('click', selectChoose)
+    });
+  
+    function selectToggle() {
+        this.parentElement.classList.toggle('is-active');
+    }
+  
+    function selectChoose() {
+        let text = this.innerText,
+            select = this.closest('.select'),
+            currentText = select.querySelector('.select-current');
+        currentText.innerText = text;
+        select.classList.remove('is-active');
+  
+    }
+    
+  };
+  
+  
+  select();
+
 });
+
+
