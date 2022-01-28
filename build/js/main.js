e651d49ec5051fd356fb01b7f9e616f0d1b5f223
OGBeas222 <zholudev111@gmail.com>
2022-01-28 16:25:53 +0300

    added mobile menu

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
   $(document).on("click", ".search-info__input", function () {
-      $(this).toggleClass(" search-info__content--clicked");
+       $(this).toggleClass(" search-info__content--clicked");
  
   });
 
+
+  $('.header-menu__btn').on('click', function(){
+    $('.menu-list').toggleClass('menu-list__active'),
+    $('.header').toggleClass('header__active',{delay:350}),
+    $('.logo').toggleClass('logo-none',{delay:350}),
+    $('.header-input').toggleClass('header-input__none'),
+    $('.header-menu__btn').toggleClass('header-menu__btn--active'),
+    $('.menu-link__footer-text').toggleClass('menu-link__footer-text--active')
+    
+  });
 
   let select = function () {
     let selectHeader = document.querySelectorAll('.select-head');
